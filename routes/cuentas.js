cb21802eb5fa5d225ae2789513b28cf848e123d8
pbussi@gmail.com <pbussi@gmail.com>
2018-08-10 21:15:05 -0300

    avances varios

--- routes/cuentas.js
+++ routes/cuentas.js
@@ -1,13 +1,56 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
+url = "http://192.168.1.107:3000"
+let options = {
+  url: "",
+  rejectUnhauthorized: 'false',
+  method: "GET",
+  headers: {
+    "content-type": "text/plain"
+  }
+};
 
 router.get('/crear', function(req, res, next) {
   res.render('cuentas_crear');
 });
 
 router.post('/crear', function(req, res, next) {
-  req.flash('successMessage', 'La cuenta '+ req.body.first_name + ' se ha creado exitosamente.');
-  res.redirect('/');
+  options.url = url + '/multichain/nuevo/'+ req.body.usuario_id;
+  request(options, (error, response, body) => {
+    if (error) {
+      req.flash('errorMessage', error);
+      res.redirect('/');
+    } else {
+      console.log(body);
+      req.flash('successMessage', 'La cuenta ' + req.body.usuario_id + ' se ha creado exitosamente. Cartera: ' + JSON.parse(body).cartera_direccion);
+      res.redirect('/');
+    }
+  });
+});
+
+router.get('/detalles/:cartera_id', function(req, res, next) {
+  options.url = url + '/multichain/saldo/'+ req.params.cartera_id;
+  request(options, (error, response, body) => {
+    if (error) {
+      req.flash('errorMessage', error);
+      res.redirect('/');
+    } else {
+      var cuenta=JSON.parse(body).result[0];
+      options.url = url + '/multichain/movimientos/'+ req.params.cartera_id;
+      request(options, (error, response, body) => {
+        if (error) {
+          req.flash('errorMessage', error);
+          res.redirect('/');
+        } else {
+          var movimientos=JSON.parse(body);
+          console.log(movimientos);
+          res.render('cuentas_detalle',{cuenta: cuenta,movimientos:movimientos,address:req.params.cartera_id});
+        }
+      });
+    }
+});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
+url = "http://192.168.1.107:3000"
+let options = {
+  url: "",
+  rejectUnhauthorized: 'false',
+  method: "GET",
+  headers: {
+    "content-type": "text/plain"
+  }
+};
+
 
 /* GET home page. */
 
-lista=[{"address" : "1MNtLQwkTRh4yiJNZ3n3RSPXTMPqEWiMVSQYWD","ismine" : true},{"address" : "19iHFpA5xoyufCpq7d3R1Tc7rbbULhUctDVcyE","ismine" : true}];
-
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Client',lista:lista});
+  options.url = url + '/multichain/cuentas';
+  request(options, (error, response, body) => {
+    if (error) {
+      res.send(error);
+    } else {
+      console.log(body);
+      res.render('index', {
+        lista: JSON.parse(body).result
+      });
+    }
+  });
 });
 
 module.exports = router;
